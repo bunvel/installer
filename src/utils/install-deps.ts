6005c91aa408afd11f773f59535c363291bdd56f
aniketkhote <aniketkhote99@gmail.com>
2025-03-03 18:39:13 +0530

    fixed framework version issue

--- src/utils/install-deps.ts
+++ src/utils/install-deps.ts
@@ -2,7 +2,7 @@ import { installPackages } from "./install";
 
 export async function installDependencies(basePath: string, db: string) {
   await installPackages(basePath, ["typescript", "@types/bun"], true);
-  await installPackages(basePath, ["@bunvel/framework@^1"]);
+  await installPackages(basePath, ["@bunvel/framework"]);
 
   if (db == "mysql") {
     await installPackages(basePath, ["mysql2"]);
